5299118ea8e448ca9abe12893befd7ea549087c1
nhatkoy <nhatkoy18gmail.com>
2024-02-13 15:57:24 +0700

    ud

--- assets/scripts/checkUserDevice.js
+++ assets/scripts/checkUserDevice.js
@@ -2,7 +2,7 @@ function redirectToMobilePage() {
     // Check if the user agent matches a mobile device and if not already on the mobile version
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) && window.location.pathname !== '/mobile.html') {
         // Redirect to the mobile version of the site
-        window.location.href = "mobile.html";
+        alert("You are being redirected to the mobile version of the site.");
     }
 }
 
