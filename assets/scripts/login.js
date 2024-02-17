31980507656d20c6d97ccdc9b6d0f129d0b153e6
Nhat Nguyen <nhatkoy18@gmail.com>
2024-02-17 17:13:54 +0700

    m

---
+++ assets/scripts/login.js
@@ -0,0 +1,4 @@
+document.getElementById("submitBtn").addEventListener("click", function(event){
+    event.preventDefault();
+        alert("Đăng nhập thành công!");
+        window.location.href = "login.html";});
